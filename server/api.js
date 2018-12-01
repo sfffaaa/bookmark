7f97c2f634310938aa773dcfb4dd65b4c171c1b5
Jay Pan <sfffaaa@gmail.com>
2018-12-01 22:19:46 +0100

    Change FHS

---
+++ server/api.js
@@ -0,0 +1,42 @@
+module.exports = (router) => {
+    router.get('/', async (req, res) => {
+        res.json({
+            success: false,
+        });
+    });
+
+    router.post('/api/create', async (req, res) => {
+        // [TODO] Need implement
+        res.json({
+            success: false,
+        });
+    });
+
+    router.post('/api/edit', async (req, res) => {
+        // [TODO] Need implement
+        res.json({
+            success: false,
+        });
+    });
+
+    router.post('/api/list', async (req, res) => {
+        // [TODO] Need implement
+        res.json({
+            success: false,
+        });
+    });
+
+    router.post('/api/delete', async (req, res) => {
+        // [TODO] Need implement
+        res.json({
+            success: false,
+        });
+    });
+
+    router.post('/api/brief', async (req, res) => {
+        // [TODO] Need implement
+        res.json({
+            success: false,
+        });
+    });
+};
--- server/server.js
+++ server/server.js
@@ -3,51 +3,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const registerApi = require('./api');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', async (req, res) => {
-    res.json({
-        success: false,
-    });
-});
-
-app.post('/api/create', async (req, res) => {
-    // [TODO] Need implement
-    res.json({
-        success: false,
-    });
-});
-
-app.post('/api/edit', async (req, res) => {
-    // [TODO] Need implement
-    res.json({
-        success: false,
-    });
-});
-
-app.post('/api/list', async (req, res) => {
-    // [TODO] Need implement
-    res.json({
-        success: false,
-    });
-});
-
-app.post('/api/delete', async (req, res) => {
-    // [TODO] Need implement
-    res.json({
-        success: false,
-    });
-});
-
-app.post('/api/brief', async (req, res) => {
-    // [TODO] Need implement
-    res.json({
-        success: false,
-    });
-});
-
+registerApi(app);
 
 module.exports = app;
