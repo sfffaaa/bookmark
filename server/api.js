b507af1826218cb7a247c01c82ffa5925f40b2eb
Jay Pan <sfffaaa@gmail.com>
2018-12-08 18:36:02 +0100

    Add try catch block for api

--- server/api.js
+++ server/api.js
@@ -11,27 +11,24 @@ module.exports = (router) => {
     });
 
     router.post('/api/create', async (req, res) => {
-        let urlInfo;
-        GetURLInfoPromise(req.body.url).then((data) => {
-            urlInfo = data;
-            return db.Bookmark.create({
-                url: data.url,
-                title: data.title,
-                description: data.description,
-                picPath: data.picPath,
+        try {
+            const urlInfo = await GetURLInfoPromise(req.body.url);
+            await db.Bookmark.create({
+                url: urlInfo.url,
+                title: urlInfo.title,
+                description: urlInfo.description,
+                picPath: urlInfo.picPath,
             });
-        /* eslint-disable-next-line no-unused-vars */
-        }).then((data) => {
             res.json({
                 data: urlInfo,
                 success: true,
             });
-        }).catch((err) => {
+        } catch (err) {
             res.json({
                 success: false,
                 err: err.message,
             });
-        });
+        }
     });
 
     router.post('/api/edit', async (req, res) => {
@@ -42,71 +39,79 @@ module.exports = (router) => {
     });
 
     router.post('/api/list', async (req, res) => {
-        const bookmarks = await db.Bookmark.findAll({});
-        res.json({
-            data: bookmarks,
-            success: true,
-        });
+        try {
+            const bookmarks = await db.Bookmark.findAll({});
+            res.json({
+                data: bookmarks,
+                success: true,
+            });
+        } catch (err) {
+            res.json({
+                success: false,
+                err: err.message,
+            });
+        }
     });
 
     router.post('/api/delete', async (req, res) => {
-        let deleteParam;
-        if (req.body.id) {
-            deleteParam = { id: req.body.id };
-        } else if (req.body.url) {
-            deleteParam = { url: req.body.url };
-        } else {
+        try {
+            let deleteParam;
+            if (req.body.id) {
+                deleteParam = { id: req.body.id };
+            } else if (req.body.url) {
+                deleteParam = { url: req.body.url };
+            } else {
+                throw new Error("url and id don't exist");
+            }
+            const bookmarks = await db.Bookmark.findAll({ where: deleteParam });
+            await db.Bookmark.destroy({ where: deleteParam });
+            if (bookmarks.length !== 0) {
+                fs.unlinkSync(bookmarks[0].picPath);
+            }
+            res.json({
+                success: true,
+            });
+        } catch (err) {
             res.json({
                 success: false,
-                message: "url and id don't exist",
+                err: err.message,
             });
-            return;
         }
-        const bookmarks = await db.Bookmark.findAll({ where: deleteParam });
-        db.Bookmark.destroy({ where: deleteParam });
-        if (bookmarks.length !== 0) {
-            fs.unlinkSync(bookmarks[0].picPath);
-        }
-        res.json({
-            success: true,
-        });
     });
 
     router.post('/api/upgrade', async (req, res) => {
-        let upgradeParam;
-        if (req.body.id) {
-            upgradeParam = { id: req.body.id };
-        } else if (req.body.url) {
-            upgradeParam = { url: req.body.url };
-        } else {
+        try {
+            let upgradeParam;
+            if (req.body.id) {
+                upgradeParam = { id: req.body.id };
+            } else if (req.body.url) {
+                upgradeParam = { url: req.body.url };
+            } else {
+                throw new Error("url and id don't exist");
+            }
+            const bookmarks = await db.Bookmark.findAll({
+                where: upgradeParam,
+            });
+            if (bookmarks.length === 0) {
+                throw new Error("url and id don't exist in database");
+            }
+            const data = GetURLInfoPromise(bookmarks[0].url);
+            await db.Bookmark.update({
+                url: data.url,
+                title: data.title,
+                description: data.description,
+                picPath: data.picPath,
+            }, {
+                where: upgradeParam,
+            });
+            res.json({
+                success: true,
+            });
+        } catch (err) {
             res.json({
                 success: false,
-                message: "url and id don't exist",
+                err: err.message,
             });
-            return;
         }
-        const bookmarks = await db.Bookmark.findAll({
-            where: upgradeParam,
-        });
-        if (bookmarks.length === 0) {
-            res.json({
-                success: false,
-                message: "url and id don't exist",
-            });
-            return;
-        }
-        const data = GetURLInfoPromise(bookmarks[0].url);
-        await db.Bookmark.update({
-            url: data.url,
-            title: data.title,
-            description: data.description,
-            picPath: data.picPath,
-        }, {
-            where: upgradeParam,
-        });
-
-        res.json({
-            success: true,
-        });
     });
 };
