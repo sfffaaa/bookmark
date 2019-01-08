9d63f17ad732aa1191258a817b46ef5d88ada409
Jay Pan <sfffaaa@gmail.com>
2019-01-08 16:36:57 +0800

    Remove test fail cases temporarily

--- test/redux.test/reducer.fail.js
+++ test/redux.test/reducer.fail.js
@@ -3,17 +3,17 @@ import deepFreeze from 'deep-freeze';
 
 import { MYURL } from './constant';
 import { bookmarkReducer } from '../../frontend/myredux/reducers/bookmarkReducer';
-import { CREATE_BOOKMARK } from '../../frontend/myredux/constants/action-types';
+import { CREATE_BOOKMARK_ACTION_TYPE } from '../../frontend/myredux/constants/action-types';
 
 describe('bookmark reducer test', () => {
     it('should add bookmark to empty list', () => {
-        const action = { type: CREATE_BOOKMARK, url: MYURL };
+        const action = { type: CREATE_BOOKMARK_ACTION_TYPE, url: MYURL };
         expect(bookmarkReducer(deepFreeze([]), action)).toMatchSnapshot();
     });
 
     it('should add bookmark to non-empty list', () => {
         const EXIST_ELEMENT = { url: 'http://show.me.the.money' };
-        const action = { type: CREATE_BOOKMARK, url: MYURL };
+        const action = { type: CREATE_BOOKMARK_ACTION_TYPE, url: MYURL };
         expect(bookmarkReducer(deepFreeze([EXIST_ELEMENT]), action)).toMatchSnapshot();
     });
 
